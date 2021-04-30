bc9966f9d66adbf73fbcba72ff083399db3d14a9
Alexandre Saudate <alesaudate@gmail.com>
2021-04-30 16:22:12 -0300

    auto-loaded

--- save.bash
+++ save.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+CURRENT_FOLDER=$(pwd)
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cp /usr/bin/release . 
-cp ~/.bash_aliases ~/workspace/my_bash_repo
+cp ~/.bash_aliases $SCRIPT_DIR
+cd $SCRIPT_DIR
 git add -A .
 git commit -m "auto-loaded"
 git push origin master
+cd $CURRENT_FOLDER
